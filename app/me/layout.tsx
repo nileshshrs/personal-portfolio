bd576a28f6b40d76e22c79380403adcb74a38f15
nileshshrs <nileshshr54@gmail.com>
2024-07-12 16:11:41 +0545

    responsive dashboard completed

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,4 +1,5 @@
 import DashboardSidebar from "@/components/DashboardSidebar";
+import ResponsiveSidebar from "@/components/ResponsiveSidebar";
 
 export default function RootLayout({
     children,
@@ -10,7 +11,13 @@ export default function RootLayout({
         <main className="flex flex-grow-4 gap-4 text-slate-400 leading-relaxed antialiased selection:bg-purple-300 selection:text-purple-900">
             <DashboardSidebar />
 
-            <div className="px-2 w-full">
+            <div className="px-2 w-full py-5">
+                <div className="inline-flex items-center justify-between gap-3 lg:gap-0">
+                    <ResponsiveSidebar />
+                    <h2 className="text-xl font-bold tracking-tight text-slate-200 sm:text-2xl">
+                        Welcome to Nilesh's Dashboard
+                    </h2>
+                </div>
                 {children}
             </div>
         </main>
