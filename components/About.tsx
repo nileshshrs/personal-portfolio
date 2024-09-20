aae96b87ec58711921ac1b1b6f37d6f4c8968e20
nileshshrs <nileshshr54@gmail.com>
2024-09-20 14:59:51 +0545

    fixed some grammer issues in the introduction

--- components/About.tsx
+++ components/About.tsx
@@ -10,11 +10,11 @@ const About = () => {
             <p className='mb-4'>
                 I'm Nilesh Shrestha, a B.Sc. (Hons.) Computing student passionate about crafting precise, engaging, and accessible digital experiences.
                 My journey into coding began in late 2020, inspired by a Discord bot that could converse with users, which was integrated using <Link href="https://www.cleverbot.com/" target="_blank" rel="noreferrer noopener" className=' text-slate-200 hover:text-purple-300 focus-visible:text-purple-300'>Cleverbot</Link>.
-                This experience opened my eyes to the possibilities of technology and sparked a desire to explore how software can enhance communication and interaction.
+                This experience sparked a desire to explore how software can enhance interactions.
 
             </p>
             <p className='mb-4'>
-                I focus on building user-centered applications using technologies like React, Java, Python, and Spring Boot. Working on various projects allows me to apply these technologies in meaningful ways.
+                Now i'm focus on building user-centered applications using technologies like React, Java, Python, and Spring Boot. Working on various projects allows me to apply these technologies in meaningful ways.
                 I'm also developing an interest in machine learning and computer vision, and I’m curious about how these technologies can enhance user experiences.
                 Through hands-on experiences, I strive to improve my skills in HTML, CSS, and JavaScript, learning to implement design best practices that contribute to accessibility and functionality.
             </p>
